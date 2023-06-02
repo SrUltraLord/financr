60f094eebc550006d9f291b6719957a5c1d78ce6
SrUltraLord <davidrey8822@gmail.com>
2023-06-02 16:38:52 -0500

    feat(button): added new variant and improved responsive.

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -2,36 +2,43 @@ import * as React from 'react'
 import { clsx } from 'clsx'
 import { type VariantProps, cva } from 'class-variance-authority'
 
-const buttonStyles = cva('rounded-md font-bold text-center transition-all', {
-  variants: {
-    variant: {
-      default: clsx([
-        'bg-brand text-white',
-        'hover:bg-indigo-600',
-        'active:bg-indigo-700',
-      ]),
-      secondary: clsx([
-        'bg-slate-200 text-black',
-        'hover:bg-slate-300',
-        'active:bg-slate-400',
-      ]),
-      ghost: clsx([
-        'border-brand border-2 text-brand',
-        'hover:bg-gray-50 hover:shadow-md hover:shadow-indigo-500/50',
-        'active:bg-gray-100',
-      ]),
+const buttonStyles = cva(
+  'inline-flex items-center justify-center rounded-md text-center transition-all cursor-default',
+  {
+    variants: {
+      variant: {
+        default: clsx([
+          'bg-brand text-white',
+          'hover:bg-indigo-600',
+          'active:bg-indigo-700',
+        ]),
+        secondary: clsx([
+          'bg-slate-200 text-black',
+          'hover:bg-slate-300',
+          'active:bg-slate-400',
+        ]),
+        outline: clsx([
+          'border-brand border-2 text-brand',
+          'hover:bg-gray-50 hover:shadow hover:shadow-indigo-500/50',
+          'active:bg-gray-100',
+        ]),
+        ghost: clsx(['bg-none', 'hover:bg-gray-50', 'active:bg-gray-100']),
+      },
+      size: {
+        lg: 'h-11 px-6 md:px-8 py-3 text-lg',
+        default: 'h-10 px-4 md:px-6 py-2 text-base',
+        sm: 'h-9 px-2 md:px-4 py-1 text-sm',
+      },
+      fullWidth: {
+        true: 'w-full',
+      },
     },
-    size: {
-      default: 'px-4 py-2',
-      sm: '',
-      lg: '',
+    defaultVariants: {
+      variant: 'default',
+      size: 'default',
     },
   },
-  defaultVariants: {
-    variant: 'default',
-    size: 'default',
-  },
-})
+)
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
